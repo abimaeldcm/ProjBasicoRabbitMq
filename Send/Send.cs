0067aeffa83b8c80013badadb2a0e613b2dd1d41
Abimael Mendes <abimaelmends@hotmail.com>
2023-11-24 13:37:36 -0300

    inicial

---
+++ Send/Send.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+while (true)
+{
+    var message = Console.ReadLine().ToString();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
